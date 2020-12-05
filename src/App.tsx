d17f792d636453e9878e1f09736e87350020d45b
Adam Witalewski <witalewski@gmail.com>
2020-12-05 17:17:21 +0100

    add ToS

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from "react";
+// import FacebookLogin from "react-facebook-login";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const responseFacebook = (response:any) => {
+    console.log(response);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <FacebookLogin
+        appId="1088597931155576"
+        autoLoad={true}
+        fields="name,email,picture"
+        onClick={console.log}
+        callback={responseFacebook}
+      /> */}
     </div>
   );
-}
+};
 
 export default App;
